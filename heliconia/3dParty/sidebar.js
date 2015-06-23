b6aa5e23c3221a6ca0bebb8a2050f1170089d076
Daniel Martinez <danielmv55@gmail.com>
2015-06-23 12:47:34 -0500

    Barra de lateral a la derecha

--- heliconia/3dParty/sidebar.js
+++ heliconia/3dParty/sidebar.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		 * This part does the "fixed navigation after scroll" functionality
 		 * We use the jQuery function scroll() to recalculate our variables as the 
 		 * page is scrolled/
-		 */
+		
 		$(window).scroll(function(){
 			var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
 			var div_top = $('#nav-anchor').offset().top;
@@ -15,7 +15,8 @@ $(document).ready(function(){
 				}
 		});
 		
-			
+		*/
+		
 		/**
 		 * This part handles the highlighting functionality.
 		 * We use the scroll functionality again, some array creation and 
